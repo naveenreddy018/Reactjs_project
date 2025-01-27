818b99967941f22a20a85afd8be0cfb2b2274d44
naveenreddy018 <naveenreddy310744@gmail.com>
2025-01-27 12:22:18 +0530

    hr34

--- src/components/authenticate/res.jsx
+++ src/components/authenticate/res.jsx
@@ -4,81 +4,40 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Navbar, Nav, Container, Button } from 'react-bootstrap';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 
-
 import Profile from '../interface/Profile';
 import AboutGeminiAI from '../interface/project';
 import Help from '../interface/Help';
 import Hello from '../main/Hello';
-import Home from "../interface/Home"
-
+import Home from "../interface/Home";
 
 function App3() {
   const [isDarkMode, setIsDarkMode] = useState(false);
-
 
   const toggleTheme = () => {
     setIsDarkMode((prevMode) => !prevMode);
   };
 
-
-  // useEffect(() => {
-  //   const token = localStorage.getItem('token'); 
-
-  //   if (!token) {
-  //     alert('Please log in first');
-  //     navigate('/login');
-  //   } else {
-  //     const sanitizedToken = token.replace(/^"|"$/g, ''); 
-  //     fetchProfile(sanitizedToken);
-  //   }
-  // }, []);
-
-
-  // const fetchProfile = async (token) => {
-  //   try {
-  //     const response = await fetch('https://render-back-end-4.onrender.com/profile', {
-  //       method: 'GET',
-  //       headers: { 'Authorization': `Bearer ${token}` },
-  //     });
-
-  //     if (response.ok) {
-  //       const data = await response.json();
-  //       console.log('Profile:', data.message);
-  //       navigate('/auth'); 
-  //     } else {
-  //       const error = await response.text();
-  //       alert(error);
-  //       navigate('/login');
-  //     }
-  //   } catch (error) {
-  //     alert('Something went wrong!');
-  //     console.error('Error fetching profile:', error);
-  //     navigate('/login');
-  //   }
-  // };
-
   return (
     <div style={isDarkMode ? styles.appContainerDark : styles.appContainerLight}>
       <AppNavbar toggleTheme={toggleTheme} isDarkMode={isDarkMode} />
-      <Home  />
+      <Home />
       <div style={styles.content}>
-      <Routes>
+        <Routes>
           <Route path="/auth" element={<App3 />} />
           <Route path="/Help" element={<Help />} />
           <Route path="/chat" element={<div style={styles.content}><Hello /></div>} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/about" element={<AboutGeminiAI />} />
-        </Routes> 
+        </Routes>
       </div>
     </div>
   );
 }
 
 function AppNavbar({ toggleTheme, isDarkMode }) {
-  const [profile, setProfile] = useState(false); 
+  const [profile, setProfile] = useState(false);
   const location = useLocation();
   const navigate = useNavigate();
-
 
   const hideNavbar =
     location.pathname === '/login' ||
@@ -86,15 +45,13 @@ function AppNavbar({ toggleTheme, isDarkMode }) {
     location.pathname === '/chat' ||
     location.pathname === '/Help';
 
-
   const handleLogout = () => {
-    localStorage.removeItem('token'); 
-    navigate('/'); 
+    localStorage.removeItem('token');
+    navigate('/');
   };
 
-
   const handleProfile = () => {
-    setProfile((prev) => !prev); 
+    setProfile((prev) => !prev);
   };
 
   return (
@@ -102,7 +59,7 @@ function AppNavbar({ toggleTheme, isDarkMode }) {
       {!hideNavbar && (
         <Navbar bg={isDarkMode ? 'dark' : 'light'} variant={isDarkMode ? 'dark' : 'light'} style={styles.navbar}>
           <Container style={styles.Container}>
-            <Navbar.Brand as={Link}  className="gemini-name" style={styles.navBrand}>
+            <Navbar.Brand as={Link} className="gemini-name" style={styles.navBrand}>
               Gemini Ai
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" style={styles.hamburger}>
@@ -117,7 +74,6 @@ function AppNavbar({ toggleTheme, isDarkMode }) {
                     Home
                   </Link>
                 </Nav.Item>
-
                 <Nav.Item>
                   <Link to="/chat" className="nav-link" style={styles.navLink}>
                     Chat
@@ -130,14 +86,23 @@ function AppNavbar({ toggleTheme, isDarkMode }) {
                 </Nav.Item>
                 <Nav.Item>
                   <Button variant="link" className="dropdown-menu" style={styles.navLink} onClick={handleProfile}>
-                    <img src="https://png.pngtree.com/png-clipart/20230927/original/pngtree-man-avatar-image-for-profile-png-image_13001882.png " width="60px" />
+                    <img
+                      src="https://png.pngtree.com/png-clipart/20230927/original/pngtree-man-avatar-image-for-profile-png-image_13001882.png"
+                      width="60px"
+                      alt="User Avatar"
+                    />
                   </Button>
-                
+
                   {profile && (
                     <div className="dropdown-menu" style={styles.profileMenu}>
-                      {/* <Button variant="link" className="dropdown-item" onClick={() => navigate('/profile')} style={styles.profileItem}>
+                      <Button
+                        variant="link"
+                        className="dropdown-item"
+                        onClick={() => navigate('/profile')}
+                        style={styles.profileItem}
+                      >
                         View Profile
-                      </Button> */}
+                      </Button>
                       <Button variant="link" className="dropdown-item" onClick={handleLogout} style={styles.profileItem}>
                         Logout
                       </Button>
@@ -145,8 +110,14 @@ function AppNavbar({ toggleTheme, isDarkMode }) {
                   )}
                 </Nav.Item>
               </Nav>
-              <Button variant={isDarkMode ? 'outline-light' : 'outline-dark'} title='Toggle button' onClick={toggleTheme} className="toggle" style={styles.toggleButton}>
-              {isDarkMode ? '🌙' : '🌞'}
+              <Button
+                variant={isDarkMode ? 'outline-light' : 'outline-dark'}
+                title="Toggle button"
+                onClick={toggleTheme}
+                className="toggle"
+                style={styles.toggleButton}
+              >
+                {isDarkMode ? '🌙' : '🌞'}
               </Button>
             </Navbar.Collapse>
           </Container>
@@ -156,106 +127,75 @@ function AppNavbar({ toggleTheme, isDarkMode }) {
   );
 }
 
-  
 const styles = {
   appContainerLight: {
     display: 'flex',
     flexDirection: 'column',
     maxHeight: '100vh',
     backgroundColor: '#000000',
-    color: 'Gold',
+    color: '#000',
   },
 
   appContainerDark: {
     display: 'flex',
     flexDirection: 'column',
     maxHeight: '100vh',
-
     backgroundColor: '#333',
-    color: '#000000',
+    color: '#ffffff',
   },
+
   navbar: {
     marginBottom: '20px',
     padding: '10px 20px',
-    backgroundColor: "#000000",
-    color: " white",
-   
+    backgroundColor: '#000',
+    color: 'white',
   },
+
   Container: {
-    padding: '0px'
+    padding: '0px',
   },
+
   navContainer: {
-    padding: '20px'
+    padding: '20px',
   },
 
   navBrand: {
     fontSize: '24px',
     fontWeight: 'bold',
     padding: '60px 0px',
-    position : "relative",
-    top : "20px",
-    left:"40px"
+    position: 'relative',
+    top: '20px',
+    left: '40px',
   },
+
   navItems: {
     display: 'flex',
     justifyContent: 'flex-end',
-    padding: '0px'
-
+    padding: '0px',
   },
+
   navLink: {
     color: '#ffffff',
     textDecoration: 'none',
     padding: '10px 15px',
     margin: '0 10px',
-    fontSize : "22px",
-    fontWeight : 400,
+    fontSize: '22px',
+    fontWeight: 400,
   },
-  navLinkHover: {
-    color: 'red',
-    textDecoration: 'underline',
-  },
-  content: {
-    flex: 0,
-    padding: '0px',
-  },
-  projectContent: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: '20px',
-    width: '100%',
-    color: "#fff"
-  },
-  imageContainer: {
-    flex: 1,
-    marginRight: '20px',
-  },
-  projectImage: {
-    width: '100%',
-    height: 'auto',
-    borderRadius: '8px',
-    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-  },
-  projectInfo: {
-    maxWidth: '600px',
-    fontSize: '16px',
-    lineHeight: '1.6',
-    color: 'white'
-  },
+
   toggleButton: {
     position: 'absolute',
-    backgroundColor : "black",
-    border : "none",
-    top : "75px",
-    left: '70%',
+    backgroundColor: 'black',
+    border: 'none',
+    top: '10%',
+    right: '30%',
     transform: 'translate(-50%, -50%)',
     zIndex: '1000',
     borderRadius: '50%',
     width: '50px',
     height: '45px',
     display: 'flex',
-    fontSize : "30px",
+    fontSize: '30px',
     justifyContent: 'center',
     alignItems: 'center',
     cursor: 'pointer',
@@ -263,7 +203,74 @@ const styles = {
     transition: 'transform 0.3s ease',
   },
 
+  content: {
+    flex: 0,
+    padding: '0px',
+  },
 
+  profileMenu: {
+    backgroundColor: 'black',
+    padding: '0px !important',
+    minWidth: '10px',
+    border: 'none',
+    top: '-10px',
+  },
+
+  profileItem: {
+    color: 'white',
+    padding: '10px 15px',
+    fontSize: '1.5rem',
+    backdropFilter: 'blur(10px)',
+    backgroundColor: 'red',
+    fontFamily: 'Times New Roman, Times, serif',
+    fontWeight: 800,
+  },
+
+  // Media Queries for responsiveness
+  '@media (max-width: 768px)': {
+    navBrand: {
+      fontSize: '18px',
+      padding: '30px 0px',
+      left: '20px',
+    },
+
+    toggleButton: {
+      top: '10%',
+      right: '5%',
+      width: '40px',
+      height: '35px',
+      fontSize: '25px',
+    },
+
+    navItems: {
+      flexDirection: 'column',
+      textAlign: 'center',
+    },
+
+    navLink: {
+      fontSize: '18px',
+      padding: '8px 10px',
+    },
+  },
+
+  '@media (max-width: 480px)': {
+    toggleButton: {
+      top: '5%',
+      right: '5%',
+      width: '40px',
+      height: '35px',
+      fontSize: '25px',
+    },
+
+    navLink: {
+      fontSize: '16px',
+    },
+
+    navBrand: {
+      fontSize: '16px',
+      left: '15px',
+    },
+  },
 };
 
 export default App3;
